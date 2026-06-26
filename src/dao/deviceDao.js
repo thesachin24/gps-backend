c0e8f98a37940204fb65eeddb7b80c02d66b926a
Sachin Chhabra <thesachin24@gmail.com>
2026-06-26 20:11:00 +0530

    fix

--- src/dao/deviceDao.js
+++ src/dao/deviceDao.js
@@ -17,7 +17,7 @@ export const getDeviceList = (filter, page, pageSize, order = []) =>
       {
         model: DeviceState,
         as: 'device_state',
-        attributes: ['latitude', 'longitude', 'speed', 'last_recorded_at']
+        attributes: ['latitude', 'longitude', 'speed', 'last_recorded_at', 'heartbeat']
       },
       {
         model: DeviceAssetMap,
