b9d7d952406df61d1493a42761e41ed804494ee1
Sachin Chhabra <thesachin24@gmail.com>
2026-07-09 21:21:57 +0530

    Fix

--- src/gps/gpsIngestionService.js
+++ src/gps/gpsIngestionService.js
@@ -84,6 +84,7 @@ export const saveHeartbeat = async ({ deviceId, parsed }) => {
     // Send push notification to the user
     // if(ignitionOn != deviceState.ignition) {
        //Notify User
+       console.log(ignitionOn, deviceState.ignition)
     void _notify(NOTIFY.IGNITION_STATE_CHANGED, device.owner_id, {
       device_name: device.device_name,
       ignition_state: ignitionOn ? 'Started' : 'Stopped',
